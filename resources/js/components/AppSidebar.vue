6dc46c97074dedd0fa3b5ed62c5be155256ff12a
sivakorn-su <101387233+sivakorn-su@users.noreply.github.com>
2025-05-09 05:59:57 +0700

    chore: Fix code

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -20,7 +20,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/sivakorn-su/toyota-exam',
         icon: Folder,
     }
 ];
